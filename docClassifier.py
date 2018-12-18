7a8cdd0fdfcf9928b53e6b4ab967521d712f8750
Ernesto Escribá Pina <Teto@Mac.local>
2018-12-18 18:57:31 +0100

    1 glosario 3 corpus

--- docClassifier.py
+++ docClassifier.py
@@ -8,8 +8,10 @@ warnings.filterwarnings(action='ignore', category=FutureWarning, module='gensim'
 from gensim import corpora, models, similarities
 from operator import itemgetter
 from sklearn.metrics.pairwise import cosine_similarity
+import csv
+import glob
 
-sample_glossary = ["deportes", "futbol", "jugar", "política", "presidente", "España", "salud", "medicina", "hospital"]
+#sample_glossary = ["deportes", "futbol", "jugar", "política", "presidente", "España", "salud", "medicina", "hospital"]
 
 sports_vector = [[1,1,1,0,0,0,0,0,0]]
 politics_vector = [[0,0,0,1,1,1,0,0,0]]
@@ -20,18 +22,19 @@ health_vector = [[0,0,0,0,0,0,1,1,1]]
 # politics_vector = [(3,1), (4,1), (5,1)]
 # health_vector = [(6,1), (7,1), (8,1)]
 
-sample_corpus = [ "Juego al futbol",
+sample_corpus = [ "Juego al fútbol",
               		"El ganador del partido de ayer, acaba en el hospital",
               		"El presidente de España ",
               		"Nueva medicina en de España ",
               		"Un partido político nuevo ",
-              		"Vaya partidazo de futbol",
+              		"Vaya partidazo del Mundial de clubs",
               		"Dame mis medicinas, quiero mis medicinas"
 				]
 
-sample_test = "Vamos a ver si jugamos un partido de futbol con y no acabo en el hospital"
+sample_test_deportes = 'Marcelo compareció en sala de prensa en la previa del duelo entre el Real Madrid y el Kashima Antlers, la segunda semifinal del Mundial de Clubes 2018 que se disputa en Abu Dabi.Mérito del Madrid: "Creo que el mérito es la unión que tenemos dentro del vestuario, el equipo lleva jugando mucho tiempo junto. Pero no se para aquí, tenemos hambre de ganar títulos, estamos en una competición corta".Nuevo reto: "No pienso en el individual, está por encima el colectivo, la unión. Somos una familia, muy amigos dentro y fuera del campo y se nota. Disputamos una competición muy corta pero que vale mucho". Mourinho: "Estoy aquí para hablar del Mundial de Clubes. Es una pena porque es un gran entrenador y ahora está sin club. No soy yo quien decide si vuelve pero le agradezco lo que hizo por mí en el Madrid".Situación de los jóvenes: "Cuando llegué al Madrid era muy joven y los que tenían experiencia me ayudaron a mí. Y nosotros con la edad que tenemos intentamos ayudar a los jóvenes. Ellos tienen que estar a gusto, trabajar y hacerlo todo para dejar al Madrid donde tiene que estar que es arriba".Habló de Isco: "Todos sabemos de la calidad, quizás el que más calidad que tiene. Se le esta dando mucha importancia a algo que no tiene mucho sentido. Algunos juegan más y otros menos. Los jugadores tenemos momentos. No hay que darle más vuelta. Está en el Real Madrid y no hace recuperarle".'
 
-sample_labels = ["deportes", "deportes", "politica", "salud", "politics", "deportes", "salud"]
+
+
 
 def preprocess_document(doc):
 	stopset = set(stopwords.words('spanish'))
@@ -73,8 +76,23 @@ def create_TF_IDF_model(docs, glossary):
 # 		print ("[ Score = " + "%.3f" % round(score,3) + "] " + corpus[doc]); 
 
 
-def read_glossary():
-    return
+def read_glossary(dir):
+	fns = glob.glob(dir+'*.csv')
+	glossary = []
+	for fn in fns:
+		with open(fn, 'r') as csvfile:
+			reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+			glossary += [row[0] for row in reader]
+	return glossary
+
+def read_corpus(dir):
+	fns = glob.glob(dir+'*.txt')
+	corpus = []
+	for fn in fns:
+		with open(fn, 'r') as txtfile:
+			content = txtfile.read()
+			corpus.append(content)
+	return corpus
 
 def label_documents():
     return
@@ -85,23 +103,24 @@ def train(corpus, glossary):
     print(tfidf)
     print("DICCIONARIO: ", dictionary)
     print("VECTORES: ", vectors)
-    index = similarities.MatrixSimilarity(vectors, num_features=len(dictionary))
-    pq = preprocess_document(sample_test)
+    index = similarities.MatrixSimilarity(tfidf[vectors], num_features=len(dictionary))
+    pq = preprocess_document(sample_test_deportes)
     vq = dictionary.doc2bow(pq)
     print("TEXTO TEST", vq)
     qtfidf = tfidf[vq]
     print("TEXTO TEST TFIDF", qtfidf)
     sim = index[qtfidf]
     
-    vq = [[0,1,1,0,0,0,0,0,1]]
-    print("GLOSARIO EJEMPLO: ", sample_glossary, "\n")
+    #vq = [[0,1,1,0,0,0,0,0,1]]
+    print("GLOSARIO EJEMPLO: ", glossary, "\n")
     print("DOCS TRAIN: ", sample_corpus, "\n")
-    print("DOC TEST: ",sample_test , "\n")
+    print("DOC TEST: ",sample_test_deportes , "\n")
     print("SIMILARIDAD CON CADA DOC:", sim, "\n")
-    print("SIM CON VEC DEPORTES ", cosine_similarity(vq,sports_vector))
-    print("SIM CON VEC POLITICA ", cosine_similarity(vq,politics_vector))
-    print("SIM CON VEC SALUD ", cosine_similarity(vq,health_vector))
+    #print("SIM CON VEC DEPORTES ", cosine_similarity(vq,sports_vector))
+    #print("SIM CON VEC POLITICA ", cosine_similarity(vq,politics_vector))
+    #print("SIM CON VEC SALUD ", cosine_similarity(vq,health_vector))
 
-
-train(sample_corpus, sample_glossary)
+glossary = read_glossary('./Glosario/')
+corpus = read_corpus('./Deportes/')
+train(corpus, glossary)
 
